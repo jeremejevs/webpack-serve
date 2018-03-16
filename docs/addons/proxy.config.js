2461ee9f107478015483454229751265ce131761
Olegs Jeremejevs <olegs@jeremejevs.com>
2018-03-16 16:48:21 +0200

    Simplify proxy addon example

--- docs/addons/proxy.config.js
+++ docs/addons/proxy.config.js
@@ -3,17 +3,6 @@
 const path = require('path');
 const proxy = require('http-proxy-middleware');
 const convert = require('koa-connect');
-const Router = require('koa-router');
-
-const router = new Router();
-
-const proxyOptions = {
-  target: 'http://api.github.com',
-  changeOrigin: true
-  // ... see: https://github.com/chimurai/http-proxy-middleware#options
-};
-
-router.get('*', convert(proxy(proxyOptions)));
 
 module.exports = {
   entry: {
@@ -28,12 +17,12 @@ module.exports = {
 module.exports.serve = {
   content: [__dirname],
   add: (app, middleware, options) => {
-    // since we're manipulating the order of middleware added, we need to handle
-    // adding these two internal middleware functions.
-    middleware.webpack();
-    middleware.content();
+    const proxyOptions = {
+      target: 'http://reqres.in/',
+      changeOrigin: true
+      // ... see: https://github.com/chimurai/http-proxy-middleware#options
+    };
 
-    // router *must* be the last middleware added
-    app.use(router.routes());
+    app.use(convert(proxy('/api', proxyOptions)));
   }
 };
