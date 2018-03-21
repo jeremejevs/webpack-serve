d6fb2e333d8e91fa4108249da0cf194252555047
Olegs Jeremejevs <olegs@jeremejevs.com>
2018-03-21 16:33:28 +0200

    Undo proxy.config.js changes, add spa-with-api-simple.config.js

--- docs/addons/proxy.config.js
+++ docs/addons/proxy.config.js
@@ -3,6 +3,17 @@
 const path = require('path');
 const proxy = require('http-proxy-middleware');
 const convert = require('koa-connect');
+const Router = require('koa-router');
+
+const router = new Router();
+
+const proxyOptions = {
+  target: 'http://api.github.com',
+  changeOrigin: true
+  // ... see: https://github.com/chimurai/http-proxy-middleware#options
+};
+
+router.get('*', convert(proxy(proxyOptions)));
 
 module.exports = {
   entry: {
@@ -17,12 +28,12 @@ module.exports = {
 module.exports.serve = {
   content: [__dirname],
   add: (app, middleware, options) => {
-    const proxyOptions = {
-      target: 'http://reqres.in/',
-      changeOrigin: true
-      // ... see: https://github.com/chimurai/http-proxy-middleware#options
-    };
+    // since we're manipulating the order of middleware added, we need to handle
+    // adding these two internal middleware functions.
+    middleware.webpack();
+    middleware.content();
 
-    app.use(convert(proxy('/api', proxyOptions)));
+    // router *must* be the last middleware added
+    app.use(router.routes());
   }
 };
